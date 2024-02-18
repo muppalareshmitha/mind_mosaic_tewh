3ddbedfa306b0482167660bb66dc3d70b1ab72fb
muppalareshmitha <muppalar@utexas.edu>
2024-02-18 11:07:03 -0600

    shape rotation demo

--- lib/pages/game_pages/shape_rotation.dart
+++ lib/pages/game_pages/shape_rotation.dart
@@ -8,13 +8,19 @@ class ShapeRotationPage extends StatefulWidget {
 }
 
 class ShapeRotationPageState extends State<ShapeRotationPage> {
+  
+  bool _showEmoji = false;
+  
   @override
   Widget build(BuildContext context) {
+    _showEmojiAfterDelay();
+
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
         actions: [
           IconButton(
-            icon: Icon(Icons.home),
+            icon: Icon(Icons.home, color: Colors.white),
             iconSize: 30,
             onPressed: () {
               Navigator.popUntil(context, ModalRoute.withName('/'));
@@ -26,10 +32,48 @@ class ShapeRotationPageState extends State<ShapeRotationPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Shape Rotation Page.'),
+            SizedBox(height: 20),
+            Icon(Icons.shape_line, size: 100, color: Colors.deepPurple),
+            SizedBox(height: 10),
+            Center(
+              child: Text(
+                'Welcome to the Shape Rotation Game!',
+                style: TextStyle(fontSize: 22),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(147, 156, 82, 230), // Faded purple color
+                borderRadius: BorderRadius.circular(20), // Rounded corners
+              ),
+              child: Text(
+                'Exercise 2: Rotate the Pentagon twice.',
+                style: TextStyle(fontSize: 20, color: Colors.white), // Text style
+              ),
+            ),
+            SizedBox(height: 20),
+            if (_showEmoji)
+              Text(
+                'Great job! 👍',
+                style: TextStyle(
+                  fontSize: 35.0, // adjust the size as needed
+                ),
+              ),
           ],
         )
       )
     );
+  }
+
+  // Method to show the emoji after a delay
+  void _showEmojiAfterDelay() {
+    // Set a delay of 5 seconds before showing the emoji
+    Future.delayed(Duration(seconds: 15), () {
+      setState(() {
+        _showEmoji = true;
+      });
+    });
   }
 }
