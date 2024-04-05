a755fab4019c6649e789aee998995fc7616946e1
muppalareshmitha <reshmitha.muppala@pairtech.ai>
2024-04-05 15:33:43 -0500

    changed home page + game_history (in progress)
    Co-authored-by: Ryan Virkar <rvirkar@users.noreply.github.com>

--- lib/pages/game_history.dart
+++ lib/pages/game_history.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class GameHistoryPage extends StatefulWidget {
   const GameHistoryPage({super.key});
@@ -8,28 +10,51 @@ class GameHistoryPage extends StatefulWidget {
 }
 
 class GameHistoryPageState extends State<GameHistoryPage> {
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  int correct = 0;
+  int incorrect = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    //_fetchUserGameData();
+  }
+
+  Future<void> _fetchUserGameData() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      DocumentSnapshot userData =
+          await _firestore.collection('users').doc(user.uid).get();
+      correct = userData.get('notematch_correct') ?? 1;
+      incorrect = userData.get('notematch_incorrect') ?? 1;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: Icon(Icons.home),
-            iconSize: 30,
-            onPressed: () {
-              Navigator.popUntil(context, ModalRoute.withName('/'));
-            },
-          ),
-        ],
+        title: Text('Game History'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Game History Page.'),
-          ],
-        )
-      )
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Text(
+      //         'Note Matching Game Correct: ${correct}',
+      //         style: TextStyle(fontSize: 18),
+      //       ),
+      //       SizedBox(),
+      //       Text(
+      //         'Note Matching Game Incorrect: ${incorrect}',
+      //         style: TextStyle(fontSize: 18),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}): super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -12,90 +12,97 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
+  Widget customButton(String text, String routeName) {
+    return SizedBox(
+      width: 170, // Set a fixed width for all buttons
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, routeName);
+        },
+        child: Text(text),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background_image.jpg'), // Adjust the path to your image
+            fit: BoxFit.cover,
+          ),
+        ),
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('Signed in as ' + user.email!),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/tutorial');
-              },
-              child: Text('Tutorial'),
+            customButton('Tutorial', '/tutorial'),
+            SizedBox(height: 20),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    customButton('Note Matching', '/note_matching'),
+                    SizedBox(width: 20),
+                    customButton('Finger Tapping', '/finger_tapping')
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    customButton('Piano Keys', '/piano_keys'),
+                    SizedBox(width:20),
+                    SizedBox(
+                      width: 170, // Set a fixed width for all buttons
+                      child: ElevatedButton(
+                      onPressed: () {
+                        //Navigator.pushNamed(context, '/reaction_time');
+                        final user = <String, dynamic>{
+                          // "uid" : FirebaseAuth.instance.currentUser?.uid ?? "",
+                          "clicks": 0
+                        };
+                        FirebaseFirestore db = FirebaseFirestore.instance;
+                        db.collection('users').add(user).then((DocumentReference doc) => print("thx"));
+                      },
+                      child: Text('Reaction Time'),
+                    )),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 170, // Set a fixed width for all buttons
+                      child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/shape_placement');
+                      },
+                      child: Text('Shape Placement'),
+                    )),
+                    SizedBox(width: 20),
+                    customButton('Shape Rotation', '/shape_rotation')
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    customButton('Simon Memory', '/simon_memory'),
+                    SizedBox(width: 20),
+                    customButton('Smash A Mole', '/smash_a_mole')
+                  ],
+                ),
+              ],
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/note_matching');
-              },
-              child: Text('Note Matching'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/finger_tapping');
-              },
-              child: Text('Finger Tapping'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/piano_keys');
-              },
-              child: Text('Piano Keys'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                //Navigator.pushNamed(context, '/reaction_time');
-                final user = <String, dynamic>{
-                // "uid" : FirebaseAuth.instance.currentUser?.uid ?? "",
-                "clicks" : 0
-              };
-              FirebaseFirestore db = FirebaseFirestore.instance;
-              db.collection('users').add(user).then((DocumentReference doc) => print("thx"));
-            },
-              child: Text('Reaction Time'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/shape_placement');
-              },
-              child: Text('Shape Placement'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/shape_rotation');
-              },
-              child: Text('Shape Rotation'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/simon_memory');
-              },
-              child: Text('Simon Memory'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/smash_a_mole');
-              },
-              child: Text('Smash A Mole'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/game_history');
-              },
-              child: Text('Game History'),
-            ),
+            customButton('Game History', '/game_history'),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
@@ -105,7 +112,8 @@ class _HomePageState extends State<HomePage> {
               child: Text('Sign Out'),
             ),
           ],
-        )),
-    );
+        ),
+      ),
+    ));
   }
 }
